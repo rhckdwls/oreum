cd8203d941eaeaa0e4b3cd92a056d3e4bb28d515
rhckdwls <rhckdwls12@naver.com>
2022-01-27 22:37:38 +0900

    make it better

---
+++ client/build/precache-manifest.6e4d42d4ac52d63d4a0f86de702cedb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0364fd98bc18ff40414a054efcd094d7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f90b47fc773c47daf687",
+    "url": "./static/css/2.0f08ce50.chunk.css"
+  },
+  {
+    "revision": "60a0057252061c29178b",
+    "url": "./static/css/main.32572983.chunk.css"
+  },
+  {
+    "revision": "f90b47fc773c47daf687",
+    "url": "./static/js/2.c26cae04.chunk.js"
+  },
+  {
+    "revision": "ffb113997df1538bba9ec71d834943f6",
+    "url": "./static/js/2.c26cae04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a0057252061c29178b",
+    "url": "./static/js/main.f83a3fa1.chunk.js"
+  },
+  {
+    "revision": "7b7c7405ff561f027886",
+    "url": "./static/js/runtime-main.6b9d98e7.js"
+  }
+]);
