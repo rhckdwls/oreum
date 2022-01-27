d5e4995d88074164e0e44bf7a8273693a82de0bf
rhckdwls <rhckdwls12@naver.com>
2022-01-27 19:19:56 +0900

    make it better

--- server/index.js
+++ server/index.js
@@ -44,13 +44,11 @@ app.use("/uploads", express.static("uploads"));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  // All the javascript and css files will be read and served from this folder
-  app.use(express.static("client/build"));
+  const root = require("path").join(__dirname, "../client,'build");
+  app.use(express.static(root));
 
-  // index.html for all page routes    html or routing and naviagtion
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile("index.html", { root });
   });
 }
 
