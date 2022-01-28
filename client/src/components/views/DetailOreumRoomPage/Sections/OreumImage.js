c5a376f315155bedf2a50597e9dd7c057a31ab94
rhckdwls <rhckdwls12@naver.com>
2022-01-29 00:11:51 +0900

    make it better

--- client/src/components/views/DetailOreumRoomPage/Sections/OreumImage.js
+++ client/src/components/views/DetailOreumRoomPage/Sections/OreumImage.js
@@ -10,7 +10,7 @@ function OreumImage(props) {
 
       props.detail.images.map((item) => {
         images.push({
-          original: `http://weoreum.herokuapp.com/${item}`,
+          fullscreen: `http://weoreum.herokuapp.com/${item}`,
         });
       });
       setImages(images);
